e6bfd17a8047ad941e5f8a3eafa97779634e6f5c
Pedro Grigorio <pedroogrigorio@gmail.com>
2024-06-19 00:12:57 -0300

    implement sections and improve responsiveness on tags scrollable container

--- src/app/student-housing/[adsId]/page.tsx
+++ src/app/student-housing/[adsId]/page.tsx
@@ -3,10 +3,15 @@
 import Image from 'next/image'
 
 import {
+  Armchair,
+  Barbell,
   Bed,
   CaretLeft,
   CaretRight,
+  Check,
   Drop,
+  Fan,
+  Garage,
   GenderFemale,
   GenderMale,
   Heart,
@@ -15,9 +20,12 @@ import {
   MapPin,
   PawPrint,
   Person,
+  ShoppingCart,
+  SwimmingPool,
   Toilet,
   Warning,
   WifiHigh,
+  X,
 } from '@phosphor-icons/react/dist/ssr'
 import { advertisements } from '@/data/advertisements'
 import {
@@ -41,24 +49,41 @@ export default function Advertisement() {
   const [rightArrowActive, setRightArrowActive] = useState(true)
 
   useEffect(() => {
-    if (scrollableContainer.current) {
-      const tabsList = scrollableContainer.current
-      tabsList.addEventListener('scroll', manageArrows)
+    const container = scrollableContainer.current
+
+    if (container) {
+      container.addEventListener('scroll', manageArrows)
+      window.addEventListener('resize', manageArrows)
+      manageArrows()
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener('scroll', manageArrows)
+        window.removeEventListener('resize', manageArrows)
+      }
     }
   }, [])
 
   const manageArrows = () => {
     if (scrollableContainer.current) {
-      const tabsList = scrollableContainer.current
-      if (tabsList.scrollLeft >= 20) {
+      const tags = scrollableContainer.current
+      console.log(tags.scrollWidth, tags.clientWidth)
+      const maxScrollValue = tags.scrollWidth - tags.clientWidth
+
+      if (maxScrollValue <= 0) {
+        setRightArrowActive(false)
+        setLeftArrowActive(false)
+        return
+      }
+
+      if (tags.scrollLeft >= 20) {
         setLeftArrowActive(true)
       } else {
         setLeftArrowActive(false)
       }
 
-      const maxScrollValue = tabsList.scrollWidth - tabsList.clientWidth - 20
-
-      if (tabsList.scrollLeft >= maxScrollValue) {
+      if (tags.scrollLeft >= maxScrollValue - 20) {
         setRightArrowActive(false)
       } else {
         setRightArrowActive(true)
@@ -93,8 +118,10 @@ export default function Advertisement() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+
       <div className="mt-10 flex flex-col text-strong">
         <div className="grid grid-cols-1 gap-6 xl:grid-cols-5 2xl:grid-cols-2">
+          {/* Foto do anúncio */}
           <div className="group relative xl:col-span-3 2xl:col-span-1">
             <Image
               src={ad.img_url}
@@ -173,16 +200,10 @@ export default function Advertisement() {
                   Possui animal de estimação
                 </span>
               </div>
-
-              <div className="flex gap-1 rounded-[4px] border border-primary bg-gray-100 p-1">
-                <PawPrint size={16} />
-                <span className="text-xs font-medium">
-                  Possui animal de estimação
-                </span>
-              </div>
             </div>
           </div>
 
+          {/* Detalhes do anúncio */}
           <div className="mt-8 flex flex-col gap-4 xl:col-span-2 xl:mt-0 2xl:col-span-1">
             <div className="flex items-start justify-between gap-4">
               <h2 className="font-bold">{ad.title}</h2>
@@ -237,6 +258,92 @@ export default function Advertisement() {
             </Button>
           </div>
         </div>
+
+        {/* Seções */}
+        <section className="mt-12 flex flex-col gap-4 py-5 xl:mt-14">
+          <div className="flex flex-col">
+            <h3 className="font-bold">Descrição</h3>
+            <div className="h-1 w-16 bg-gray-800" />
+          </div>
+
+          <p>
+            Este quarto que estamos anunciando tem 9 m2 e conta com Cama de
+            casal , Armário 2 portas, e banheiro exclusivo. Ele fica em um
+            apartamento espaçoso, de 90 m2, e está bem pertinho do metrô Campo
+            Belo - linha lilas e de locais como Flavour Lab e Pizzaria do
+            Baldoíno. Você poderá morar com até mais 1 pessoa, mas, não se
+            preocupe: o valor da sua mensalidade será sempre o mesmo. Para
+            facilitar ainda mais a sua vida, você paga todas as contas do mês
+            juntas, num boleto único.
+          </p>
+        </section>
+
+        <div className="h-[1px] w-full bg-divisor" />
+
+        <section className="flex flex-col gap-4 py-5">
+          <div>
+            <div className="flex flex-col">
+              <h3 className="font-bold">Comodidades</h3>
+              <div className="h-1 w-16 bg-gray-800" />
+            </div>
+          </div>
+
+          <div
+            className="grid grid-cols-3 gap-y-8"
+            style={{
+              gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr)',
+            }}
+          >
+            <div className="flex items-center gap-2">
+              <Armchair size={20} />
+              Residência mobiliada
+            </div>
+            <div className="flex items-center gap-2">
+              <Garage size={20} />
+              Vaga em garagem
+            </div>
+            <div className="flex items-center gap-2">
+              <Fan size={20} />
+              Ar condicionado
+            </div>
+            <div className="flex items-center gap-2">
+              <SwimmingPool size={20} />
+              Piscina
+            </div>
+            <div className="flex items-center gap-2">
+              <Barbell size={20} />
+              Academia
+            </div>
+            <div className="flex items-center gap-2">
+              <ShoppingCart size={20} />
+              Mercado próximo
+            </div>
+          </div>
+        </section>
+
+        <div className="h-[1px] w-full bg-divisor" />
+
+        <section className="flex flex-col gap-4 py-5">
+          <div className="flex flex-col">
+            <h3 className="font-bold">Regras</h3>
+            <div className="h-1 w-16 bg-gray-800" />
+          </div>
+
+          <ul>
+            <li className="flex gap-2">
+              <Check weight="bold" size={24} className="text-gray-500" />
+              <span>Moradores podem receber visitas</span>
+            </li>
+            <li className="flex gap-2">
+              <X weight="bold" size={24} className="text-danger" />
+              <span>Proibido fumar</span>
+            </li>
+            <li className="flex gap-2">
+              <X weight="bold" size={24} className="text-danger" />
+              <span>Proibido bebidas alcóolicas</span>
+            </li>
+          </ul>
+        </section>
       </div>
     </div>
   )
