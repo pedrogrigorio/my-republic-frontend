949bc6f2a20a4861090584d18640e17c6be12ebc
Pedro Grigorio <pedroogrigorio@gmail.com>
2024-10-07 18:56:13 -0300

    remove use form

--- src/components/forms/locale-search-form/index.tsx
+++ src/components/forms/locale-search-form/index.tsx
@@ -4,28 +4,19 @@ import { ChangeEvent, useState } from 'react'
 import { searchCitiesByTerm } from '@/services/locale-service'
 import { MagnifyingGlass } from '@phosphor-icons/react/dist/ssr'
 import { useDebounce } from '@/hooks/useDebounce'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
 import { useQuery } from '@tanstack/react-query'
-import { useForm } from 'react-hook-form'
 import { Spinner } from '@nextui-org/spinner'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { City } from '@/types/city'
 import { cn } from '@/lib/utils'
-import { z } from 'zod'
 
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover'
-
-const localeSearchFormSchema = z.object({
-  search: z.string(),
-})
-
-type localeSearchFormData = z.infer<typeof localeSearchFormSchema>
 
 interface LocaleSearchFormProps {
   className?: string
@@ -48,17 +39,11 @@ export default function LocaleSearchForm({
 
   const isButtonDisabled = isLoading || search !== debounceSearch
 
-  const { register, handleSubmit, reset } = useForm<localeSearchFormData>({
-    resolver: zodResolver(localeSearchFormSchema),
-  })
-
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value)
   }
 
   const onSearch = () => {
-    reset()
-
     if (cities && cities.length > 0) {
       router.push(`/student-housing/search/${cities[0].id}`)
     }
@@ -70,10 +55,7 @@ export default function LocaleSearchForm({
   }
 
   return (
-    <form
-      className="mt-4 flex flex-1 justify-center gap-3"
-      onSubmit={handleSubmit(onSearch)}
-    >
+    <form className="mt-4 flex flex-1 justify-center gap-3" onSubmit={onSearch}>
       <div className={cn('flex-1', className)}>
         <Popover open={cities && cities.length > 0 && !!search} modal={false}>
           <PopoverTrigger asChild>
@@ -84,8 +66,8 @@ export default function LocaleSearchForm({
                 type="search"
                 className=" border-none placeholder:text-placeholder focus-visible:ring-0 focus-visible:ring-offset-0"
                 placeholder="Busque por cidade"
+                onChange={handleChange}
                 value={search}
-                {...register('search', { onChange: (e) => handleChange(e) })}
               />
             </div>
           </PopoverTrigger>
